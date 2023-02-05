b168777b13637797050a91545c9cae2c010e3968
Wasath Theekshana <wasath.vt@gmail.com>
2023-02-05 15:09:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Spline from "@splinetool/react-spline";
+
+function App() {
+  return (
+    <div className="App">
+      <div style={{height:'100vh'}}>
+        <Spline scene="https://prod.spline.design/HYLbZ1h30psyGrfG/scene.splinecode" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
